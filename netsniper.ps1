6e89b34fdf4806ab44e387dcdfe1964bb6756188
jli2011 <jli2011@users.noreply.github.com>
2016-02-29 10:36:03 -1000

    Update netsniper.ps1
    Notifies Admin if device is online or offiline

--- netsniper.ps1
+++ netsniper.ps1
@@ -1,35 +1,30 @@
+Import-Csv 'ipCSV.csv' |
+
 getmac #prints out mac address and transport name of current system
 arp -a #prints out all mac addresses
 $macdata = arp -a #puts all mac addresses into variable
+Clear-Content Desktop\test.txt #erase contents of a file
 Add-Content Desktop\test.txt $macdata #appends all mac addresses to file
-write-host $macdata[0] #prints out the first line of macdata
-write-host $macdata[1] #prints out the first line of macdata
-write-host $macdata[2] #prints out the first line of macdata
-write-host $macdata[3] #prints out the first line of macdata
-write-host $macdata[4] #prints out the first line of macdata
-write-host $macdata[5] #prints out the first line of macdata
-write-host $macdata[6] #prints out the first line of macdata
 
-$ipInput = Read-Host -Prompt 'Please enter the IP Address you want to monitor (in 000.000.000.000 format)' #prompts user to input the ip address to monitor
+#gets ip address from mac address
 $macInput = Read-Host -Prompt 'Please enter the MAC Address you want to monitor (in 00-00-00-00-00-00 format)' #prompts user to input the mac address to monitor
-$typeInput = Read-Host -Prompt 'Please enter the type' #prompts user to input the type
-
-$compareInput = '  ' $ipInput ''
-
+write-host " "
 write-host "You input the MAC address" $macInput #displays user input for user to double check
+$ipAddress = arp -a | select-string $macInput |% { $_.ToString().Trim().Split(" ")[0] } #gets the ip address from the MAC
+write-host "The ip address of this MAC address is" $ipAddress #prints the IP address from MAC address input
 
-$i = 0
-while ($i = 0)
-{
-    #compares the line with the macInput to each line in the macdata
-    #if it reaches the end and can't find the line, then email admin and say device is offline
-    #if found, email admin that device is online
-}
+#ping $ipAddress >> Desktop\ping.txt $pingData #continuous ping
+
+if(Test-Connection $ipAddress -count 4 -quiet) #pings the ip address 4 times
+    {
+        write-host "Device Online." #if ping successful
+    }
+else
+    {
+        write-host "Device Offline." #if ping failed
+    }
 
 #sends an email to specified address
-$PSEmailServer = "careercenter.colo.hawaii.edu" #smtp server name
-Send-MailMessage -From "jiajie@hawaii.edu" -To "jiajie@hawaii.edu" -Subject "Test email" -Body "This is a test email" #email info
-
-128.171.112.1         70-ca-9b-78-8f-c3     dynamic
+Send-MailMessage -From "jiajie@hawaii.edu" -To "jiajie@hawaii.edu" -Subject "Test email" -Body "This is a test email" -SmtpServer "smtp.gmail.com" #email info
 
 
