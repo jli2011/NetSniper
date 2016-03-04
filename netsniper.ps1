e91128b7a7519fed67f285a6d858dc525b865bf6
jli2011 <jli2011@users.noreply.github.com>
2016-03-04 11:26:41 -1000

    Update netsniper.ps1
    Takes status and prints out offline/online based on true/false

--- netsniper.ps1
+++ netsniper.ps1
@@ -1,13 +1,11 @@
-Import-Csv 'ipCSV.csv' |
-
 getmac #prints out mac address and transport name of current system
 arp -a #prints out all mac addresses
 $macdata = arp -a #puts all mac addresses into variable
 write-host " "
 write-host "The size of macdata is" $macdata.Length #prints out the length of macdata
 
-#Clear-Content Desktop\test.txt #erase contents of a file
-#Add-Content Desktop\test.txt $macdata #appends all mac addresses to file
+Clear-Content Desktop\test.txt #erase contents of a file
+Add-Content Desktop\test.txt $macdata #appends all mac addresses to file
 
 #gets ip address from mac address
 $macInput = Read-Host -Prompt 'Please enter the MAC Address you want to monitor (in 00-00-00-00-00-00 format)' #prompts user to input the mac address to monitor
@@ -16,14 +14,16 @@ write-host "You input the MAC address" $macInput #displays user input for user t
 
 $i = 1
 
-while($i = 1)
-{
     $ipAddress = arp -a | select-string $macInput |% { $_.ToString().Trim().Split(" ")[0] } #gets the ip address from the MAC
     write-host "The ip address of this MAC address is" $ipAddress #prints the IP address from MAC address input
 
+while($i = 1)
+{
     #ping $ipAddress >> Desktop\ping.txt $pingData #continuous ping
+    
+    $status = Test-Connection $ipAddress -count 4 -quiet
 
-        if (Test-Connection $ipAddress -count 4 -quiet) #pings the ip address 4 times
+        if ($status = "True") #pings the ip address 4 times
             {
                 write-host "Device Online." #if ping successful
                 $macdata = arp -a
