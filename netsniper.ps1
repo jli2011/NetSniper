9f0b9530de6d962dc3c9c566e54676aaebd75de1
jli2011 <jli2011@users.noreply.github.com>
2016-03-10 10:44:16 -1000

    Update netsniper.ps1
    Working netSniper. Scans Mac address and gives online status. If offline, dialog box appears.

--- netsniper.ps1
+++ netsniper.ps1
@@ -1,13 +1,7 @@
-getmac #prints out mac address and transport name of current system
-arp -a #prints out all mac addresses
-$macArray = arp -a #puts all mac addresses into array
-write-host " "
-write-host "The size of macArray is" $macArray.Length #prints out the length of macdata
-
-Clear-Content Desktop\test.txt #erase contents of a file
-Add-Content Desktop\test.txt $macArray #appends all mac addresses to file
-
-$ErrorActionPreference = "Stop"
+#getmac #prints out mac address and transport name of current system
+#arp -a #prints out all mac addresses
+#Clear-Content Desktop\test.txt #erase contents of a file
+#Add-Content Desktop\test.txt $macArray #appends all mac addresses to file
 
 #gets ip address from mac address
 $macInput = Read-Host -Prompt 'Please enter the MAC Address you want to monitor (in 00-00-00-00-00-00 format)' #prompts user to input the mac address to monitor
@@ -15,53 +9,40 @@ write-host " "
 write-host "You input the MAC address" $macInput #displays user input for user to double check
 
 $i = 1
+$checkNumber = 1
 
 while($i = 1)
 {
+    $buttonNumber = 0
+    $macArray = arp -a #puts all mac addresses into array
+    write-host " "
+    write-host "There are" $macArray.Length "ARP entries" #prints out the length of macdata
     $ipAddress = arp -a | select-string $macInput |% { $_.ToString().Trim().Split(" ")[0] } #gets the ip address from the MAC
     
-    if ([string]::IsNullOrEmpty($ipAddress))
+    if ([string]::IsNullOrEmpty($ipAddress))#if MAC address is not found
         {
             write-host "IP address not found"
-            write-host "Online status = False"
+            
+            #displays error message box
             $wshell = New-Object -ComObject Wscript.Shell
-            $wshell.Popup("Device not found!",0,"Error Message")
-            Exit
+            $buttonNumber = $wshell.Popup("Device offline!",0,"Device Status")
+            write-host "Device is offline"
+            
+            Exit #terminates code 
         }
-    else
+    else #if MAC address is found
         {
-        $status = Test-Connection $ipAddress -count 4 -quiet -ErrorAction Stop
+        $status = Test-Connection $ipAddress -count 4 -quiet #ping the ip address 4 times while suppressing all errors
         
-        if ($status = "True")
+        if ($status = "True") #if connection returns true
             {
                 write-host "The ip address of this MAC address is" $ipAddress #prints the IP address from MAC address input
-            }
-        else 
-            {
-                write-host "That MAC address is not online"
-                $1 = 0
+                write-host "Connection check number = 4 x" $checkNumber #each connection check is equal to 4 pings
+                $checkNumber = $checkNumber + 1
+                write-host "Device Online." #if ping successful
+                write-host " "
             }
         }
-    
-    write-host "Online status =" $status
-    
-    #ping $ipAddress >> Desktop\ping.txt $pingData #continuous ping
-    
-    $status = Test-Connection $ipAddress -count 4 -quiet -ErrorAction Stop
-
-        if ($status = "True") #pings the ip address 4 times
-            {
-                write-host "Device Online." #if ping successful
-                $macArray = arp -a
-            }
-        else
-            {
-                write-host "Device Offline." #if ping failed
-                $macArray = arp -a
-            }
 }
-
-#sends an email to specified address
-#Send-MailMessage -From "jiajie@hawaii.edu" -To "jiajie@hawaii.edu" -Subject "Test email" -Body "This is a test email" -SmtpServer "smtp.gmail.com" #email info
 
 
